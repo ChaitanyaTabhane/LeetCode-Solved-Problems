dccf196b1af28a2fa8286f09936b463b428965b2
ChaitanyaTabhane <71386171+ChaitanyaTabhane@users.noreply.github.com>
2022-07-11 21:49:48 +0530

    Time: 5 ms (39.23%), Space: 9.1 MB (71.80%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
-//         //for edge case i.e. if the nums1 is empty then just append the elements of another array
+//         //for edge case i.e. if the nums1 is empty then just add the elements of another array
 //         if(m==0){
 //              for(int i=0; i<n;i++){
 //                  nums1[i]=nums2[i]+nums1[i];
