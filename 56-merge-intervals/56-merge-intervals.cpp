97d2cf9fb87b88e8a870eab585ee72b3d2c81879
ChaitanyaTabhane <71386171+ChaitanyaTabhane@users.noreply.github.com>
2022-07-11 10:12:38 +0530

    Time: 59 ms (50.80%), Space: 20.1 MB (26.05%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        vector<vector<int>> ans;
+        
+        //checking if the vector is empty
+        if(intervals.size() == 0) {
+            return ans; 
+        }
+        //sorting the vector
+        sort(intervals.begin(), intervals.end());
+        vector<int> temp = intervals[0]; 
+        
+    
+        for(auto it : intervals) {
+            // checking if the 2nd element of the first pair is less than the 1st element in the second row 
+            if(it[0] <= temp[1]) {
+                temp[1] = max(it[1], temp[1]);// replacing the element to maximum 
+            } else {
+                ans.push_back(temp); 
+                temp = it; 
+            }
+        }
+        ans.push_back(temp);
+        return ans; 
+    }
+};
