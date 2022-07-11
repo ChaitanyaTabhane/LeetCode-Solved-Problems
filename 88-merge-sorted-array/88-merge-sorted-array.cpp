62b31c11d5fb977db7423ec059882abf097ee501
ChaitanyaTabhane <71386171+ChaitanyaTabhane@users.noreply.github.com>
2022-07-11 21:48:48 +0530

    Time: 11 ms (5.33%), Space: 9.2 MB (29.14%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if(m==0){
-             for(int i=0; i<n;i++){
-                 nums1[i]=nums2[i]+nums1[i];
-             }
-        }
         
-         else if(n!=0){
-             for(int i=0; i<n;i++){
-                nums1[i+m]=nums2[i]+nums1[i+m];
-                 cout<<nums1[i]<<" ";
-             }
-             sort(nums1.begin(),nums1.end());
-         }        
+//         //for edge case i.e. if the nums1 is empty then just append the elements of another array
+//         if(m==0){
+//              for(int i=0; i<n;i++){
+//                  nums1[i]=nums2[i]+nums1[i];
+//              }
+//         }
+         
+//         //if the second array is having elements
+//         else if(n!=0){
+//             for(int i=0; i<n;i++){
+//                 // add the elements to the zeros of nums1
+//                 nums1[i+m]=nums2[i]+nums1[i+m];
+//              }
+//             //sort the array
+//              sort(nums1.begin(),nums1.end());
+//          }
+        // complexity is O(nlogn)
+        
+        int p1=m-1, p2 =n-1, i=m+n-1;
+        while(p2>=0){
+            if(p1 >= 0 && nums1[p1]>nums2[p2]){
+                nums1[i--] = nums1[p1--];
+            }
+            else{
+                nums1[i--] = nums2[p2--];
+            }
+        }
     }
 };
